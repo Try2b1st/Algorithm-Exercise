30a81d26f47b2236a116f9e74a86812fd96a9c83
Wgz <1808365012@mail.ynu.edu.cn>
2023-09-18 15:21:16 +0800

    904. 水果成篮

--- src/main/java/org/array/SlidingWindow.java
+++ src/main/java/org/array/SlidingWindow.java
@@ -30,4 +30,53 @@ public class SlidingWindow {
         }
         return result;
     }
+
+
+    /**
+     * 904. 水果成篮
+     *
+     * @param fruits
+     * @return
+     */
+    public int totalFruit(int[] fruits) {
+        if (fruits.length == 1) {
+            return 1;
+        }
+        if (fruits.length == 2) {
+            return 2;
+        }
+        int r = fruits[0];
+        int l = -1;
+        int count = 1;
+        int windowSize = 1;
+        int result = 0;
+        int start = 0;
+        for (int end = 1; end < fruits.length; end++) {
+            if (count == 1) {
+                if (fruits[end] != r) {
+                    l = r;
+                    r = fruits[end];
+                    count++;
+                }
+                windowSize++;
+                result = Math.max(result, windowSize);
+            } else {
+                if (fruits[end] != r && fruits[end] != l) {
+                    result = Math.max(result, end - start);
+                    r = fruits[end];
+                    l = fruits[end - 1];
+                    start = end - 2;
+                    while (fruits[start] == l) {
+                        start--;
+                    }
+                    start++;
+                    windowSize = end - start + 1;
+                } else {
+                    windowSize++;
+                    result = Math.max(result, windowSize);
+                }
+            }
+        }
+        return result;
+    }
 }
