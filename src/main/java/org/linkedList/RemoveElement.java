38d963ff8e79df1dc0037ffaa9e16e2a08347e0c
Wgz <1808365012@mail.ynu.edu.cn>
2023-09-19 16:51:03 +0800

    203. 移除链表元素

---
+++ src/main/java/org/linkedList/RemoveElement.java
@@ -0,0 +1,50 @@
+package org.linkedList;
+
+/**
+ * @author 下水道的小老鼠
+ */
+public class RemoveElement {
+    /**
+     * 基本定义
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    /**
+     * 203. 移除链表元素
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode nonHead = new ListNode(-1, head);
+        ListNode pre = nonHead;
+        while (head != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+            } else {
+                pre = head;
+            }
+            head = head.next;
+        }
+        return nonHead.next;
+    }
+}
