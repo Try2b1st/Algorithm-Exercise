fc8646e2c73d6efce738097c807d38e221156388
Wgz <1808365012@mail.ynu.edu.cn>
2023-09-19 17:54:54 +0800

    707. 设计链表

--- src/main/java/org/linkedList/RemoveElement.java
+++ src/main/java/org/linkedList/RemoveElement.java
@@ -36,15 +36,91 @@ public class RemoveElement {
             return null;
         }
         ListNode nonHead = new ListNode(-1, head);
-        ListNode pre = nonHead;
+        ListNode pred = nonHead;
         while (head != null) {
             if (head.val == val) {
-                pre.next = head.next;
+                pred.next = head.next;
             } else {
-                pre = head;
+                pred = head;
             }
             head = head.next;
         }
         return nonHead.next;
+    }
+
+    /**
+     * 707. 设计链表
+     */
+    public static class MyLinkedList {
+        //size存储链表元素的个数
+        int size;
+        //虚拟头结点
+        ListNode head;
+
+        //初始化
+        public MyLinkedList() {
+            size = 0;
+            head = new ListNode(0);
+        }
+
+        //获取第index个节点的数值，注意index是从0开始的，第0个节点就是头结点
+        public int get(int index) {
+            if (index < 0 || size <= index) {
+                return -1;
+            }
+            ListNode currentHead = head;
+            for (int i = 0; i <= index; i++) {
+                currentHead = currentHead.next;
+            }
+            return currentHead.val;
+        }
+
+        public void addAtHead(int val) {
+            addAtIndex(0, val);
+        }
+
+        //在链表的最后插入一个节点，等价于在(末尾+1)个元素前添加
+        public void addAtTail(int val) {
+            addAtIndex(size, val);
+        }
+
+        // 在第 index 个节点之前插入一个新节点，例如index为0，那么新插入的节点为链表的新头节点。
+        // 如果 index 等于链表的长度，则说明是新插入的节点为链表的尾结点
+        // 如果 index 大于链表的长度，则返回空
+        public void addAtIndex(int index, int val) {
+            if (index > size) {
+                return;
+            }
+            if (index < 0) {
+                index = 0;
+            }
+            size++;
+
+            ListNode pred = head;
+            for (int i = 0; i < index; i++) {
+                pred = pred.next;
+            }
+            ListNode newNode = new ListNode(val);
+            newNode.next = pred.next;
+            pred.next = newNode;
+        }
+
+        //删除第index个节点
+        public void deleteAtIndex(int index) {
+            if (index < 0 || index >= size) {
+                return;
+            }
+            size--;
+            if (index == 0) {
+                head = head.next;
+                return;
+            }
+
+            ListNode pred = head;
+            for (int i = 0; i < index; i++) {
+                pred = pred.next;
+            }
+            pred.next = pred.next.next;
+        }
     }
 }
