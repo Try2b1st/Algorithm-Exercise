50d1bccda9b894ebd930d2c780a661068136ac62
Wgz <1808365012@mail.ynu.edu.cn>
2023-09-23 14:07:54 +0800

    349. 两个数组的交集

---
+++ src/main/java/org/hash/AboutHashSet.java
@@ -0,0 +1,29 @@
+package org.hash;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * @author 下水道的小老鼠
+ */
+public class AboutHashSet {
+
+    /**
+     * 349. 两个数组的交集
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if(nums1[i] == nums2[j]){
+                    hashSet.add(nums1[i]);
+                }
+            }
+        }
+        return hashSet.stream().mapToInt(x -> x).toArray();
+    }
+}
