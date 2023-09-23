0d44c70589a0e5b582e8597ceead553a597a0a90
Wgz <1808365012@mail.ynu.edu.cn>
2023-09-23 16:51:52 +0800

    349. 两个数组的交集

--- src/main/java/org/hash/AboutHashSet.java
+++ src/main/java/org/hash/AboutHashSet.java
@@ -10,20 +10,43 @@ public class AboutHashSet {
 
     /**
      * 349. 两个数组的交集
+     * 暴力解法
      *
      * @param nums1
      * @param nums2
      * @return
      */
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersectionBad(int[] nums1, int[] nums2) {
         HashSet<Integer> hashSet = new HashSet<>();
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
-                if(nums1[i] == nums2[j]){
+                if (nums1[i] == nums2[j]) {
                     hashSet.add(nums1[i]);
                 }
             }
         }
         return hashSet.stream().mapToInt(x -> x).toArray();
     }
+
+    /**
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> resultSet = new HashSet<>();
+        HashSet<Integer> nums1Set = new HashSet<>();
+        for (int i : nums1) {
+            nums1Set.add(i);
+        }
+
+        for (int i : nums2) {
+            if (nums1Set.contains(i)) {
+                resultSet.add(i);
+            }
+        }
+        return resultSet.stream().mapToInt(x -> x).toArray();
+    }
+
+
 }
