0d1701855736d43ec8bba790789979420abe9f0c
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-09 15:04:41 +0800

    迭代

--- src/main/java/org/tree/Traversal.java
+++ src/main/java/org/tree/Traversal.java
@@ -2,6 +2,7 @@ package org.tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * @author 下水道的小老鼠
@@ -34,8 +35,34 @@ public class Traversal {
      * @return
      */
     public List<Integer> preorderTraversal(TreeNode root) {
+        /**
+         * 递归
+         */
+//        List<Integer> result = new ArrayList<>();
+//        preorder(root, result);
+//        return result;
+
+        /**
+         * 迭代
+         */
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> result = new ArrayList<>();
-        preorder(root, result);
+        if (root == null) {
+            return result;
+        }
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            result.add(current.val);
+            if (current != null) {
+                if (current.right != null) {
+                    stack.push(current.right);
+                }
+                if (current.left != null) {
+                    stack.push(current.left);
+                }
+            }
+        }
         return result;
     }
 
@@ -56,8 +83,32 @@ public class Traversal {
      * @return
      */
     public List<Integer> inorderTraversal(TreeNode root) {
+        /**
+         * 递归
+         */
+//        List<Integer> result = new ArrayList<>();
+//        inorder(root, result);
+//        return result;
+
+        /**
+         * 迭代
+         */
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> result = new ArrayList<>();
-        inorder(root, result);
+        if (root == null) {
+            return result;
+        }
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            if (current != null) {
+                stack.push(current);
+                current = current.left;
+            } else {
+                current = stack.pop();
+                result.add(current.val);
+                current = current.right;
+            }
+        }
         return result;
     }
 
@@ -78,9 +129,39 @@ public class Traversal {
      * @return
      */
     public List<Integer> postorderTraversal(TreeNode root) {
+        /**
+         * 递归
+         */
+//        List<Integer> result = new ArrayList<>();
+//        postorder(root, result);
+//        return result;
+
+        /**
+         * 迭代
+         */
+        Stack<TreeNode> stack = new Stack<>();
         List<Integer> result = new ArrayList<>();
-        postorder(root, result);
+        if (root == null) {
+            return result;
+        }
+        TreeNode current = root;
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            if (current != null) {
+                while (current.left != null) {
+                    stack.push(current.left);
+                    current = current.left;
+                }
+                current = stack.pop();
+                result.add(current.val);
+
+                if (current.right != null) {
+                    stack.push(current.right);
+                }
+            }
+        }
         return result;
+
     }
 
     public void postorder(TreeNode root, List<Integer> list) {
