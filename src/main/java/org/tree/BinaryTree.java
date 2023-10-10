b87aed0d51c7ea5f8c252ae054c8920b7dd5366a
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-10 20:19:51 +0800

    101. 对称二叉树

--- src/main/java/org/tree/BinaryTree.java
+++ src/main/java/org/tree/BinaryTree.java
@@ -1,5 +1,6 @@
 package org.tree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -49,4 +50,38 @@ public class BinaryTree {
         root.left = root.right;
         root.right = temp;
     }
+
+
+    /**
+     * 101. 对称二叉树
+     *
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return compare(root.left, root.right);
+    }
+
+    public boolean compare(TreeNode left, TreeNode right) {
+
+        if (left != null && right == null) {
+            return false;
+        } else if (left == null && right != null) {
+            return false;
+        } else if (right == null && left == null) {
+            return true;
+        } else if (left.val != right.val) {
+            return false;
+        }
+        //外
+        boolean leftFlag = compare(left.left, right.right);
+        //里
+        boolean rightFlag = compare(left.right, right.left);
+
+        return leftFlag && rightFlag;
+    }
+
 }
