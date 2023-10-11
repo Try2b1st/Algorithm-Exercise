2abad96b0d6ee4593d248c4c360863b0e3461bd5
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-11 17:35:46 +0800

    110. 平衡二叉树

--- src/main/java/org/tree/BinaryTree.java
+++ src/main/java/org/tree/BinaryTree.java
@@ -169,7 +169,26 @@ public class BinaryTree {
      * @return
      */
     public boolean isBalanced(TreeNode root) {
+        if (getHeight(root) == -1) {
+            return false;
+        }
+        return true;
+    }
 
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        if (leftHeight == -1 || rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.abs(leftHeight - rightHeight);
     }
 
 }
