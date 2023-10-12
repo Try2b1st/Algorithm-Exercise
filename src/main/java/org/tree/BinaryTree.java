17fddd738d25d00f2c4bd955d8b55224fb2bfd85
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-12 11:24:21 +0800

    404. 左叶子之和

--- src/main/java/org/tree/BinaryTree.java
+++ src/main/java/org/tree/BinaryTree.java
@@ -200,7 +200,7 @@ public class BinaryTree {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<>();
         List<String> path = new ArrayList<>();
-        traversal(root,path,result);
+        traversal(root, path, result);
         return result;
     }
 
@@ -224,6 +224,32 @@ public class BinaryTree {
             traversal(root.right, path, result);
             path.remove(path.size() - 1);
         }
+    }
+
+    /**
+     * 404. 左叶子之和
+     *
+     * @param root
+     * @return
+     */
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int leftValue = sumOfLeftLeaves(root.left);    // 左
+        int rightValue = sumOfLeftLeaves(root.right);  // 右
+
+        int midValue = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            midValue = root.left.val;
+        }
+        int sum = midValue + leftValue + rightValue;  // 中
+        return sum;
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+
     }
 
 }
