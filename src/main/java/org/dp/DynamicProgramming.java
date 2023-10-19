d67ee019e65800cb988b162b86e241316a4be269
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-19 15:36:31 +0800

    63. 不同路径 II

--- src/main/java/org/dp/DynamicProgramming.java
+++ src/main/java/org/dp/DynamicProgramming.java
@@ -97,4 +97,58 @@ public class DynamicProgramming {
 
         return array[n - 1];
     }
+
+    /**
+     * 63. 不同路径 II
+     *
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int x = obstacleGrid[0].length;
+        int y = obstacleGrid.length;
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[y - 1][x - 1] == 1) {
+            return 0;
+        } else {
+            obstacleGrid[0][0] = 1;
+        }
+
+        for (int i = 1; i < x; i++) {
+            if (obstacleGrid[0][i] == 1) {
+                while (i < x) {
+                    obstacleGrid[0][i] = 0;
+                    i++;
+                }
+                break;
+            } else {
+                obstacleGrid[0][i] = 1;
+            }
+        }
+
+        for (int i = 1; i < y; i++) {
+            if (obstacleGrid[i][0] == 1) {
+                while (i < y) {
+                    obstacleGrid[i][0] = 0;
+                    i++;
+                }
+                break;
+            } else {
+                obstacleGrid[i][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < y; i++) {
+            for (int j = 1; j < x; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    obstacleGrid[i][j] = 0;
+                } else {
+                    obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                }
+            }
+        }
+
+
+        return obstacleGrid[y - 1][x - 1];
+    }
+
 }
