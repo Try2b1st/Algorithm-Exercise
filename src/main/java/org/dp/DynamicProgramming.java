9d2258774397f2ef9189e7cdcf2018864d60ee71
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-19 15:01:52 +0800

    62. 不同路径

--- src/main/java/org/dp/DynamicProgramming.java
+++ src/main/java/org/dp/DynamicProgramming.java
@@ -1,14 +1,13 @@
 package org.dp;
 
 public class DynamicProgramming {
-    /**
-     * 对于动态规划问题，我将拆解为如下五步曲，这五步都搞清楚了，才能说把动态规划真的掌握了！
-     *
-     * 1.确定dp数组（dp table）以及下标的含义
-     * 2.确定递推公式
-     * 3.dp数组如何初始化
-     * 4.确定遍历顺序
-     * 5.举例推导dp数组
+    /*
+      对于动态规划问题，我将拆解为如下五步曲，这五步都搞清楚了，才能说把动态规划真的掌握了！
+      1.确定dp数组（dp table）以及下标的含义
+      2.确定递推公式
+      3.dp数组如何初始化
+      4.确定遍历顺序
+      5.举例推导dp数组
      */
 
     /**
@@ -72,8 +71,30 @@ public class DynamicProgramming {
         dp[0] = 0;
 
         for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.min(dp[i - 1] + cost[i-1], dp[i - 2] + cost[i - 2]);
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
         }
         return dp[size];
+    }
+
+    /**
+     * 62. 不同路径
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                array[j] += array[j - 1];
+            }
+        }
+
+        return array[n - 1];
     }
 }
