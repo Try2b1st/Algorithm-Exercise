40055d2a9c80127c99b1d9398b51fe7df0bad975
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-20 14:12:56 +0800

    96. 不同的二叉搜索树

--- src/main/java/org/dp/DynamicProgramming.java
+++ src/main/java/org/dp/DynamicProgramming.java
@@ -171,4 +171,25 @@ public class DynamicProgramming {
         return dp[n];
     }
 
+    /**
+     * 96. 不同的二叉搜索树
+     *
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
 }
