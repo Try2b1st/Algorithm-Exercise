6629b5ae3dbf1fa2558da7b2e54a815b6792c6a6
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-23 12:06:55 +0800

    494. 目标和

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -16,18 +16,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         DynamicProgramming dynamicProgramming = new DynamicProgramming();
-        Scanner sc = new Scanner(System.in);
-        int length  = sc.nextInt();
-        int bagSize = sc.nextInt();
-        int[] weight = new int[length];
-        int[] values = new int[length];
-        for(int i = 0; i< length;i++){
-            weight[i] = sc.nextInt();
-        }
-        for(int i = 0; i< length;i++){
-            values[i] = sc.nextInt();
-        }
-
-        dynamicProgramming.bagOneQuestion(weight,values,bagSize);
+        int[] nums = new int[]{1,1,1};
+        dynamicProgramming.findTargetSumWays(nums,1);
     }
 }
--- src/main/java/org/dp/DynamicProgramming.java
+++ src/main/java/org/dp/DynamicProgramming.java
@@ -282,9 +282,9 @@ public class DynamicProgramming {
 
         for (int i = 1; i < stones.length; i++) {
             for (int j = 1; j < avg + 1; j++) {
-                if(j >= stones[i]){
+                if (j >= stones[i]) {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - stones[i]] + stones[i]);
-                }else{
+                } else {
                     dp[i][j] = dp[i - 1][j];
                 }
             }
@@ -301,6 +301,27 @@ public class DynamicProgramming {
      * @return
      */
     public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if ((sum + target) % 2 == 1) {
+            return 0;
+        }
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        int[] dp = new int[(sum + target) / 2 + 1];
 
+        //初始化
+        dp[0] = 1;
+
+        for (int num : nums) {
+            for (int j = dp.length - 1; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[dp.length - 1];
     }
 }
