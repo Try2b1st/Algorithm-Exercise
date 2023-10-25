6831ffc434a2473f9b119f0489f2b0b5e6cd7064
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-25 18:06:09 +0800

    股票问题

---
+++ src/main/java/org/dp/ShareProblem.java
@@ -0,0 +1,14 @@
+package org.dp;
+
+public class ShareProblem {
+
+    /**
+     * 121. 买卖股票的最佳时机
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
