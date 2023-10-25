7e4b05f89a6bbcef5ee7f63f7db6984226159c5d
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-25 18:03:22 +0800

    337. 打家劫舍 III

--- src/main/java/org/dp/Rob.java
+++ src/main/java/org/dp/Rob.java
@@ -37,7 +37,7 @@ public class Rob {
         int left = robRange(nums, 0, nums.length - 2);
         int right = robRange(nums, 1, nums.length - 1);
 
-        return Math.max(left,right);
+        return Math.max(left, right);
     }
 
     public int robRange(int[] nums, int start, int end) {
@@ -54,5 +54,54 @@ public class Rob {
         }
 
         return dp[dp.length - 1];
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    /**
+     * 337. 打家劫舍 III
+     *
+     * @param root
+     * @return
+     */
+    public int robIII(TreeNode root) {
+        int[] result = traversal(root);
+        return Math.max(result[0], result[1]);
+    }
+
+    public int[] traversal(TreeNode cur) {
+        if (cur == null) {
+            return new int[]{0, 0};
+        }
+
+        //0-不偷 1-偷
+        int[] result = new int[2];
+
+        int[] left = traversal(cur.left);
+        int[] right = traversal(cur.right);
+
+        //不偷当前节点
+        result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+
+        //偷当前节点
+        result[1] = cur.val + left[0] + right[0];
+        return result;
     }
 }
