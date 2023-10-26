40f36065026552d0748e5af4747f59b5bc5caf62
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-26 14:41:47 +0800

    123. 买卖股票的最佳时机 III

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -9,6 +9,6 @@ import org.dp.ShareProblem;
 public class Main {
     public static void main(String[] args) {
         ShareProblem shareProblem = new ShareProblem();
-        System.out.println(shareProblem.maxProfitII(new int[]{7,1,5,3,6,4}));
+        System.out.println(shareProblem.maxProfitIII(new int[]{1,2,3,4,5}));
     }
 }
--- src/main/java/org/dp/ShareProblem.java
+++ src/main/java/org/dp/ShareProblem.java
@@ -19,7 +19,7 @@ public class ShareProblem {
 
         for (int i = 1; i < prices.length; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
-            dp[i][1] = Math.max(prices[i] + dp[i][0], dp[i][1]);
+            dp[i][1] = Math.max(prices[i] + dp[i - 1][0], dp[i - 1][1]);
         }
         return dp[prices.length - 1][1];
     }
@@ -38,8 +38,40 @@ public class ShareProblem {
 
         for (int i = 1; i < prices.length; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = Math.max(prices[i] + dp[i][0], dp[i - 1][1]);
+            dp[i][1] = Math.max(prices[i] + dp[i - 1][0], dp[i - 1][1]);
         }
         return dp[prices.length - 1][1];
+    }
+
+    /**
+     * 123. 买卖股票的最佳时机 III
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfitIII(int[] prices) {
+        /*
+        没有操作 （其实我们也可以不设置这个状态）
+        第一次持有股票
+        第一次不持有股票
+        第二次持有股票
+        第二次不持有股票
+         */
+
+        int[][] dp = new int[prices.length][5];
+        dp[0][1] = -prices[0];
+        dp[0][2] = 0;
+        dp[0][3] = -prices[0];
+        dp[0][4] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            dp[i][2] = Math.max(dp[i - 1][2], dp[i - 1][1] + prices[i]);
+            dp[i][3] = Math.max(dp[i - 1][3], dp[i - 1][2] - prices[i]);
+            dp[i][4] = Math.max(dp[i - 1][4], dp[i - 1][3] + prices[i]);
+        }
+        System.out.println(Arrays.deepToString(dp));
+        return dp[prices.length - 1][4];
+
     }
 }
