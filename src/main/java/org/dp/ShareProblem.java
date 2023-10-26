bb8215834c0c22b18e05d4b9737217a208278cd3
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-26 11:01:51 +0800

    121. 买卖股票的最佳时机

--- src/main/java/org/dp/ShareProblem.java
+++ src/main/java/org/dp/ShareProblem.java
@@ -9,6 +9,16 @@ public class ShareProblem {
      * @return
      */
     public int maxProfit(int[] prices) {
-        return 0;
+
+        //初始化
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0],-prices[i]);
+            dp[i][1] = Math.max(0,Math.max(prices[i] + dp[i][0],dp[i][1]));
+        }
+        return dp[prices.length - 1][1];
     }
 }
