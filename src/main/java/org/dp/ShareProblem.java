4cd0dc53fa28d0ba00a9af945482782e45f17272
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-27 09:57:40 +0800

    714. 买卖股票的最佳时机含手续费

--- src/main/java/org/dp/ShareProblem.java
+++ src/main/java/org/dp/ShareProblem.java
@@ -131,7 +131,27 @@ public class ShareProblem {
             dp[i][3] = dp[i - 1][2];
             System.out.println(Arrays.toString(dp[i]));
         }
-
         return Math.max(dp[prices.length - 1][1], Math.max(dp[prices.length - 1][2], dp[prices.length - 1][3]));
+    }
+
+
+    /**
+     * 714. 买卖股票的最佳时机含手续费
+     *
+     * @param prices
+     * @param fee
+     * @return
+     */
+    public int maxProfit(int[] prices, int fee) {
+        //初始化
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
+            dp[i][1] = Math.max(prices[i] + dp[i - 1][0] - fee, dp[i - 1][1]);
+        }
+        return dp[prices.length - 1][1];
     }
 }
