9678665f9d2098fb4135299c65755392a682c2ea
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-28 12:20:02 +0800

    674. 最长连续递增序列

--- src/main/java/org/dp/SubQueue.java
+++ src/main/java/org/dp/SubQueue.java
@@ -20,13 +20,13 @@ public class SubQueue {
         System.out.println();
         for (int i = 0; i < dp.length; i++) {
             for (int j = 0; j < i; j++) {
-                if(nums[j] < nums[i]){
-                    dp[i] = Math.max(dp[i],1 + dp[j]);
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j]);
                 }
             }
         }
         System.out.println(Arrays.toString(dp));
-        int result= 0;
+        int result = 0;
         for (int j : dp) {
             if (result < j) {
                 result = j;
@@ -42,30 +42,23 @@ public class SubQueue {
      * @return
      */
     public int findLengthOfLCIS(int[] nums) {
-
+        //初始化
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < dp.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                dp[i] = Math.max(dp[i], 1 + dp[i - 1]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        int result = 0;
+        for (int j : dp) {
+            if (result < j) {
+                result = j;
+            }
+        }
+        return result;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
