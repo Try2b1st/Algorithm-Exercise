37f9a80414f11dc04374d2e9286632ccb1f77692
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-28 12:37:38 +0800

    718. 最长重复子数组

--- src/main/java/org/dp/SubQueue.java
+++ src/main/java/org/dp/SubQueue.java
@@ -69,6 +69,35 @@ public class SubQueue {
      * @return
      */
     public int findLength(int[] nums1, int[] nums2) {
-        return 0;
+        //确定dp和初始化
+        int[][] dp = new int[nums1.length][nums2.length];
+        for (int i = 0; i < nums2.length; i++) {
+            if (nums1[0] == nums2[i]) {
+                dp[0][i] = 1;
+            }
+        }
+        for (int i = 0; i < nums1.length; i++) {
+            if (nums1[i] == nums2[0]) {
+                dp[i][0] = 1;
+            }
+        }
+
+        for (int i = 1; i < nums1.length; i++) {
+            for (int j = 1; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if(result < dp[i][j]){
+                    result = dp[i][j];
+                }
+            }
+        }
+        return result;
     }
 }
