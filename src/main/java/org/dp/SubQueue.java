70d05096d7ab29029e2d507c82b2649f247b20df
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-29 10:39:41 +0800

    392. 判断子序列

--- src/main/java/org/dp/SubQueue.java
+++ src/main/java/org/dp/SubQueue.java
@@ -165,11 +165,37 @@ public class SubQueue {
         }
 
         int result = Integer.MIN_VALUE;
-        for(int x : dp){
-            if(x>result){
+        for (int x : dp) {
+            if (x > result) {
                 result = x;
             }
         }
         return result;
+    }
+
+
+    /**
+     * 392. 判断子序列
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() > t.length()) {
+            return false;
+        }
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 1; i < s.length() + 1; i++) {
+            for (int j = 1; j < t.length() + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[s.length()][t.length()] == s.length();
     }
 }
