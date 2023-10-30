1dd42e01fda33ca20732e847dc53d2c1ad47b604
Wgz <1808365012@mail.ynu.edu.cn>
2023-10-30 15:10:44 +0800

    72. 编辑距离

--- src/main/java/org/dp/SubQueue.java
+++ src/main/java/org/dp/SubQueue.java
@@ -233,11 +233,11 @@ public class SubQueue {
      * @param word2
      * @return
      */
-    public int minDistance(String word1, String word2) {
+    public int minDistanceI(String word1, String word2) {
         int[][] dp = new int[word1.length() + 1][word2.length() + 1];
 
-        for (int i = 1; i <= word1.length() + 1; i++) dp[i][0] = i;
-        for (int j = 1; j <= word2.length() + 1; j++) dp[0][j] = j;
+        for (int i = 1; i < word1.length() + 1; i++) dp[i][0] = i;
+        for (int j = 1; j < word2.length() + 1; j++) dp[0][j] = j;
 
         for (int i = 1; i < word1.length() + 1; i++) {
             for (int j = 1; j < word2.length() + 1; j++) {
@@ -245,6 +245,32 @@ public class SubQueue {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = Math.min(dp[i - 1][j - 1] + 2, Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
+
+    /**
+     * 72. 编辑距离
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistanceII(String word1, String word2) {
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+
+        for (int i = 1; i < word1.length() + 1; i++) dp[i][0] = i;
+        for (int j = 1; j < word2.length() + 1; j++) dp[0][j] = j;
+
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1] + 1, Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1));
                 }
             }
         }
