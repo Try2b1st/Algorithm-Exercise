02d9af91315364346a8e22a1b156fcbcf942db4d
Wgz <1808365012@mail.ynu.edu.cn>
2023-11-05 12:54:32 +0800

    47. 全排列 II

--- src/main/java/org/Track/Permutations.java
+++ src/main/java/org/Track/Permutations.java
@@ -39,4 +39,36 @@ public class Permutations {
             used[i] = false;
         }
     }
+
+    /**
+     * 47. 全排列 II
+     *
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        backtrackingToPermuteUnique(nums, used);
+        return result;
+    }
+
+    public void backtrackingToPermuteUnique(int[] nums, boolean[] used) {
+        if (list.size() == nums.length) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            if (!used[i]) {
+                used[i] = true;
+                list.add(nums[i]);
+                backtrackingToPermute(nums, used);
+                list.remove(list.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
 }
