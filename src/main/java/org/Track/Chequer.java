dec22026dc73e14fa8c5c1eeebb7df3a5b3e1a9a
Wgz <1808365012@mail.ynu.edu.cn>
2023-11-07 12:56:58 +0800

    37. 解数独

--- src/main/java/org/Track/Chequer.java
+++ src/main/java/org/Track/Chequer.java
@@ -13,6 +13,7 @@ public class Chequer {
      * @return
      */
     List<List<String>> result = new ArrayList<>();
+
     public List<List<String>> solveNQueens(int n) {
         char[][] chessboard = new char[n][n];
         for (char[] c : chessboard) {
@@ -29,7 +30,7 @@ public class Chequer {
         }
 
         for (int i = 0; i < n; i++) {
-            if(isValid(n,row,i,chessboard)){
+            if (isValidSolveNQueens(n, row, i, chessboard)) {
                 chessboard[row][i] = 'Q';
                 backtrackingToSolveNQueens(n, row + 1, chessboard);
                 chessboard[row][i] = '.';
@@ -38,7 +39,7 @@ public class Chequer {
     }
 
     //验证合法性
-    public boolean isValid(int n, int row, int col, char[][] chessboard) {
+    public boolean isValidSolveNQueens(int n, int row, int col, char[][] chessboard) {
         //检查列
         for (int i = 0; i < n; i++) {
             if (chessboard[i][col] == 'Q') {
@@ -48,14 +49,14 @@ public class Chequer {
 
         //检查45°斜线
         for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if(chessboard[i][j]=='Q'){
+            if (chessboard[i][j] == 'Q') {
                 return false;
             }
         }
 
         //检查135°斜线
         for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if(chessboard[i][j]=='Q'){
+            if (chessboard[i][j] == 'Q') {
                 return false;
             }
         }
@@ -75,5 +76,62 @@ public class Chequer {
         }
         return result;
     }
+
+
+    /**
+     * 37. 解数独
+     *
+     * @param board
+     */
+    public void solveSudoku(char[][] board) {
+        backtrackingToSolveSudoku(board);
+    }
+
+    public boolean backtrackingToSolveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char k = '1'; k <= '9'; k++) {
+                        if (isValidSolveSudoku(k, i, j, board)) {
+                            board[i][j] = k;
+                            if (backtrackingToSolveSudoku(board)) {
+                                return true;
+                            }
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValidSolveSudoku(char number, int row, int col, char[][] board) {
+        //检查列
+        for (int i = 0; i < 9; i++) {
+            if (number == board[i][col]) {
+                return false;
+            }
+        }
+        //检查行
+        for (int i = 0; i < 9; i++) {
+            if (number == board[row][i]) {
+                return false;
+            }
+        }
+        //检查小的九方格
+        int startRow = (row / 3) * 3;
+        int startCol = (col / 3) * 3;
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == number) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
 }
