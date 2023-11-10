97d28ff650e34c99f409813893f1f3bb7840be58
Wgz <1808365012@mail.ynu.edu.cn>
2023-11-10 10:43:40 +0800

    11.10 每日一题

--- src/main/java/org/Daily.java
+++ src/main/java/org/Daily.java
@@ -1,15 +1,38 @@
 package org;
 
+import java.util.Arrays;
+
 public class Daily {
 
     /**
-     * 11.09 每日一题
-     * 2258. 逃离火灾
+     * 11.10 每日一题
+     * 2300. 咒语和药水的成功对数
      *
-     * @param grid
+     * @param spells
+     * @param potions
+     * @param success
      * @return
      */
-    public int maximumMinutes(int[][] grid) {
-        return 0;
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions);
+
+        int[] result = new int[spells.length];
+
+        for (int i = 0; i < spells.length; i++) {
+            int start = 0;
+            int end = potions.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+
+                if ((long) potions[mid] * spells[i] >= success) {
+                    result[i] = potions.length - mid;
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return result;
     }
 }
