ed32e0063df15c4669e1f2959cd58cfe8e65981b
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-10 23:57:54 +0800

    12.10 每日一题 70. 爬楼梯

--- src/main/java/org/Daily.java
+++ src/main/java/org/Daily.java
@@ -950,4 +950,24 @@ public class Daily {
         return balance[i];
     }
 
+
+    /**
+     * 12.10 每日一题
+     * 70. 爬楼梯
+     *
+     * @param n
+     * @return
+     */
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j <= 2; j++) {
+                dp[i] += dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
 }
