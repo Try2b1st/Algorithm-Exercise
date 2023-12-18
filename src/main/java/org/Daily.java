85166827ebdad93ee0b30d24e1acf41e6cc91210
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-18 15:07:59 +0800

    12.18 每日一题 162. 寻找峰值

--- src/main/java/org/Daily.java
+++ src/main/java/org/Daily.java
@@ -1178,4 +1178,27 @@ public class Daily {
         return root;
     }
 
+
+    /**
+     * 12.18 每日一题
+     * 162. 寻找峰值
+     *
+     * @param nums
+     * @return
+     */
+    public int findPeakElement(int[] nums) {
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1] > nums[nums.length-2]){
+            return nums.length - 1;
+        }
+        for(int i = 1; i<nums.length -1;i++){
+            if(nums[i] > nums[i - 1] && nums[i] > nums[i+1]){
+                return i;
+            }
+        }
+        return 0;
+    }
+
 }
