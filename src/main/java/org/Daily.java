ef30dbdc00a6903e823a5a8b3820052b90a05198
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-21 11:45:00 +0800

    12.21 每日一题 2866. 美丽塔 II

--- src/main/java/org/Daily.java
+++ src/main/java/org/Daily.java
@@ -1190,11 +1190,11 @@ public class Daily {
         int n = nums.length;
         int l = 0;
         int r = nums.length - 1;
-        while(l < r){
+        while (l < r) {
             int mid = l + r >> 1;
-            if(nums[mid] < nums[mid + 1]){
+            if (nums[mid] < nums[mid + 1]) {
                 l = mid + 1;
-            }else{
+            } else {
                 r = mid;
             }
         }
@@ -1211,9 +1211,52 @@ public class Daily {
      */
     public boolean isAcronym(List<String> words, String s) {
         StringBuilder stringBuilder = new StringBuilder();
-        for(String word : words){
+        for (String word : words) {
             stringBuilder.append(word.charAt(0));
         }
         return s.equals(stringBuilder.toString());
+    }
+
+
+    /**
+     * 12.21 每日一题
+     * 2866. 美丽塔 II
+     *
+     * @param maxHeights
+     * @return
+     */
+    public long maximumSumOfHeights(List<Integer> maxHeights) {
+        int n = maxHeights.size();
+        long res = 0;
+        long[] prefix = new long[n];
+        long[] suffix = new long[n];
+        Deque<Integer> stack1 = new ArrayDeque<Integer>();
+        Deque<Integer> stack2 = new ArrayDeque<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack1.isEmpty() && maxHeights.get(i) < maxHeights.get(stack1.peek())) {
+                stack1.pop();
+            }
+            if (stack1.isEmpty()) {
+                prefix[i] = (long) (i + 1) * maxHeights.get(i);
+            } else {
+                prefix[i] = (long) (i - stack1.peek()) * maxHeights.get(i) + prefix[stack1.peek()];
+            }
+            stack1.push(i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack2.isEmpty() && maxHeights.get(i) < maxHeights.get(stack2.peek())) {
+                stack2.pop();
+            }
+            if (stack2.isEmpty()) {
+                suffix[i] = (long) (n - i) * maxHeights.get(i);
+            } else {
+                suffix[i] = suffix[stack2.peek()] + (long) (stack2.peek() - i) * maxHeights.get(i);
+            }
+            stack2.push(i);
+            res = Math.max(res, prefix[i] + suffix[i] - maxHeights.get(i));
+        }
+        return res;
     }
 }
