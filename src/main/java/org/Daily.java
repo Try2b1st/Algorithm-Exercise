aa68d1e2e988229835f2bc22c5edd9e1c09bb6aa
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-22 09:06:11 +0800

    12.22 每日一题 1671. 得到山形数组的最少删除次数

--- src/main/java/org/Daily.java
+++ src/main/java/org/Daily.java
@@ -1259,4 +1259,51 @@ public class Daily {
         }
         return res;
     }
+
+
+    /**
+     * 12.22 每日一题
+     * 1671. 得到山形数组的最少删除次数
+     *
+     * @param nums
+     * @return
+     */
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] pre = getLISArray(nums);
+        int[] reverse = reverse(nums);
+        int[] suf = getLISArray(reverse);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (pre[i] > 1 && suf[n - 1 - i] > 1) {
+                ans = Math.max(ans, pre[i] + suf[n - 1 - i] - 1);
+            }
+        }
+        return n - ans;
+    }
+
+    public int[] getLISArray(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+
+    public int[] reverse(int[] nums) {
+        int n = nums.length;
+        int[] reversed = new int[n];
+        for (int i = 0; i < n; i++) {
+            reversed[i] = nums[n - 1 - i];
+        }
+        return reversed;
+    }
 }
