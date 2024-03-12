017a8de3f8c2eec304f46b0108c155e51743f7be
Wgz <1808365012@mail.ynu.edu.cn>
2024-03-12 20:57:37 +0800

    LCR 168. 丑数

--- src/main/java/LCR/hash/Hash.java
+++ src/main/java/LCR/hash/Hash.java
@@ -319,6 +319,34 @@ public class Hash {
         }
         return stringBuilder.toString();
     }
+
+    /**
+     * LCR 168. 丑数
+     *
+     * @param n
+     * @return
+     */
+    public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int a = 0, b = 0, c = 0;
+
+        for (int i = 1; i < n; i++) {
+            int n2 = res[a] * 2, n3 = res[b] * 3, n5 = res[c] * 5;
+            res[i] = Math.min(n2, Math.min(n3, n5));
+            if (res[i] == n2) {
+                a++;
+            }
+            if (res[i] == n3) {
+                b++;
+            }
+            if (res[i] == n5) {
+                c++;
+            }
+        }
+
+        return res[n - 1];
+    }
 }
 
 
