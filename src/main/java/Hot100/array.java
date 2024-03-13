9df44cdd9dd7d02df897b2b72555bddd4af7a56e
Wgz <1808365012@mail.ynu.edu.cn>
2024-03-13 11:06:48 +0800

    189. 轮转数组

---
+++ src/main/java/Hot100/array.java
@@ -0,0 +1,29 @@
+package Hot100;
+
+public class array {
+    /**
+     * 189. 轮转数组
+     *
+     * @param nums
+     * @param k
+     */
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums,0,nums.length - 1);
+        reverse(nums,0, k - 1);
+        reverse(nums,k,nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        int l = start;
+        int r = end;
+
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
