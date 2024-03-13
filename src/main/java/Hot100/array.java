89b7acbd76c24f129110b7cd3048e26e11b7acf9
Wgz <1808365012@mail.ynu.edu.cn>
2024-03-13 11:34:46 +0800

    238. 除自身以外数组的乘积

--- src/main/java/Hot100/array.java
+++ src/main/java/Hot100/array.java
@@ -9,9 +9,9 @@ public class array {
      */
     public void rotate(int[] nums, int k) {
         k %= nums.length;
-        reverse(nums,0,nums.length - 1);
-        reverse(nums,0, k - 1);
-        reverse(nums,k,nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
     }
 
     public void reverse(int[] nums, int start, int end) {
@@ -25,5 +25,30 @@ public class array {
             l++;
             r--;
         }
+    }
+
+
+    /**
+     * 238. 除自身以外数组的乘积
+     *
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] result = new int[length];
+
+        result[0] = 1;
+        for (int i = 1; i < length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        int temp = 1;
+        for (int i = length - 2; i >= 0; i--) {
+            temp *= nums[i+1];
+            result[i] *= temp;
+        }
+
+        return result;
     }
 }
