bb138a742d456f30e3e8321db7c1d7e2df745c84
Wgz <1808365012@mail.ynu.edu.cn>
2024-03-28 16:21:11 +0800

    LCR 137. 模糊搜索验证

--- src/main/java/LCR/dp/Question.java
+++ src/main/java/LCR/dp/Question.java
@@ -1,5 +1,7 @@
 package LCR.dp;
 
+import java.util.Arrays;
+
 public class Question {
 
     int MOD = 1000000007;
@@ -57,11 +59,39 @@ public class Question {
      * @return
      */
     public boolean articleMatch(String s, String p) {
-        int n = s.length();
-        int m = p.length();
-        boolean[][] dp = new boolean[n + 1][m + 1];
-
-
-        return dp[n][m];
+        int m = s.length() + 1, n = p.length() + 1;
+        boolean[][] dp = new boolean[m][n];
+        dp[0][0] = true;
+        // 初始化首行
+        for(int j = 2; j < n; j += 2)
+            dp[0][j] = dp[0][j - 2] && p.charAt(j - 1) == '*';
+        // 状态转移
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(p.charAt(j - 1) == '*') {
+                    if(dp[i][j - 2]) dp[i][j] = true;                                            // 1.
+                    else if(dp[i - 1][j] && s.charAt(i - 1) == p.charAt(j - 2)) dp[i][j] = true; // 2.
+                    else if(dp[i - 1][j] && p.charAt(j - 2) == '.') dp[i][j] = true;             // 3.
+                } else {
+                    if(dp[i - 1][j - 1] && s.charAt(i - 1) == p.charAt(j - 1)) dp[i][j] = true;  // 1.
+                    else if(dp[i - 1][j - 1] && p.charAt(j - 1) == '.') dp[i][j] = true;         // 2.
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/org/ex/Main.java
+++ src/main/java/org/ex/Main.java
@@ -13,14 +13,9 @@ public class Main {
     public static void main(String[] args) {
         Question question = new Question();
         Daily daily = new Daily();
-        LCR.string.Question question1 = new LCR.string.Question();
+        LCR.dp.Question question1 = new LCR.dp.Question();
+        question1.articleMatch("aaa","aaaa");
 
-        int[] postorder = new int[]{4,9,6,5,8};
-        int[] post = postorder;
-        Arrays.sort(postorder);
-        int[] mid = postorder;
-        System.out.println(Arrays.toString(post));
-        System.out.println(Arrays.toString(mid));
     }
 }
 
