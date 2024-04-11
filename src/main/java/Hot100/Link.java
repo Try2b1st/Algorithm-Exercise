5005aefa6c9f8aa43f9acf183bd4d95d49704128
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-11 15:06:51 +0800

    23. 合并 K 个升序链表

--- src/main/java/Hot100/Link.java
+++ src/main/java/Hot100/Link.java
@@ -1,5 +1,8 @@
 package Hot100;
 
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Link {
     public class ListNode {
         int val;
@@ -20,7 +23,7 @@ public class Link {
 
 
     /**
-     *
+     * 25. K 个一组翻转链表
      *
      * @param head
      * @param k
@@ -34,7 +37,7 @@ public class Link {
 
         ListNode pre = null;
         ListNode cur = head;
-        ListNode dummy = new ListNode(0,head);
+        ListNode dummy = new ListNode(0, head);
         ListNode p = dummy;
 
         for (; n >= k; n -= k) {
@@ -52,5 +55,35 @@ public class Link {
             p = nx;
         }
         return dummy.next;
+    }
+
+    /**
+     * 23. 合并 K 个升序链表
+     *
+     * @param lists
+     * @return
+     */
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+
+        for(ListNode head : lists){
+            if(head !=null){
+                pq.offer(head);
+            }
+        }
+
+        ListNode head = new ListNode();
+        ListNode cur = head;
+
+        while(!pq.isEmpty()){
+            ListNode temp = pq.poll();
+            if(temp.next != null){
+                pq.offer(temp.next);
+            }
+            cur.next = temp;
+            cur = cur.next;
+        }
+
+        return head.next;
     }
 }
