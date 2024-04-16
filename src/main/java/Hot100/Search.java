c725a929d661c3efab429e9a11ce317603104207
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-16 17:57:58 +0800

    33. 搜索旋转排序数组

--- src/main/java/Hot100/Search.java
+++ src/main/java/Hot100/Search.java
@@ -17,15 +17,59 @@ public class Search {
         int y = 0;
 
         while (x > 0 && y < m) {
-            if(matrix[x][y] == target){
+            if (matrix[x][y] == target) {
                 return true;
             }
-            if(matrix[x][y] > target){
+            if (matrix[x][y] > target) {
                 x--;
-            }else{
+            } else {
                 y++;
             }
         }
         return false;
+    }
+
+
+    /**
+     * 33. 搜索旋转排序数组
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int rightStart = findMin(nums);
+        if(nums[n - 1] > target){
+            return findTarget(nums,-1,rightStart,target);
+        }
+        return findTarget(nums,rightStart - 1,n,target);
+    }
+
+    private int findMin(int[] nums) {
+        int n = nums.length;
+        int left = -1;
+        int right = n - 1;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[n - 1]) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+
+    private int findTarget(int[] nums,int left,int right,int target){
+        while(left + 1 < right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[right] == target ? right : -1;
     }
 }
