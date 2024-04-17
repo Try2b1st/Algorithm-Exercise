1f895b095031021c840b019861716c4e00f674e6
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-17 17:05:22 +0800

    118. 杨辉三角

---
+++ src/main/java/Hot100/Dp.java
@@ -0,0 +1,45 @@
+package Hot100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dp {
+
+    /**
+     * 118. 杨辉三角
+     *
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        //初始化
+        List<Integer> origin1 = new ArrayList<>();
+        origin1.add(1);
+        result.add(origin1);
+        if(numRows == 1){
+            return result;
+        }
+
+        List<Integer> origin2 = new ArrayList<>();
+        origin2.add(1);
+        origin2.add(1);
+        result.add(origin2);
+        if(numRows == 2){
+            return result;
+        }
+
+        for (int i = 0; i < numRows - 2; i++) {
+            List<Integer> list = new ArrayList<>();
+            List<Integer> temp = result.get(i + 1);
+            list.add(1);
+            for (int j = 0; j <= i; j++) {
+                list.add(temp.get(j) + temp.get(j + 1));
+            }
+            list.add(1);
+            result.add(list);
+        }
+        return result;
+    }
+}
