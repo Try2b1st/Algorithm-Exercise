669865f3453b8f16ee3aff3d24ee5a12360fd2a7
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-18 15:26:00 +0800

    169. 多数元素

--- src/main/java/Hot100/Skill.java
+++ src/main/java/Hot100/Skill.java
@@ -1,5 +1,7 @@
 package Hot100;
 
+import java.util.Arrays;
+
 public class Skill {
 
     /**
@@ -13,6 +15,24 @@ public class Skill {
         for (int num : nums)  // 1. 遍历 nums 执行异或运算
             x ^= num;
         return x;            // 2. 返回出现一次的数字 x
+    }
+
+
+    /**
+     * 169. 多数元素
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int ans = 0;
+        int count = 0;
+
+        for (int x : nums) {
+            if (count == 0) ans = x;
+            count += x == ans ? 1 : -1;
+        }
+        return ans;
     }
 }
 
