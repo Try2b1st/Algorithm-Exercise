da4c39e394cb033cbe861e84a0d424fa089029ba
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-24 18:19:54 +0800

    4.24 2385. 感染二叉树需要的总时间

--- src/main/java/org/ex/Daily.java
+++ src/main/java/org/ex/Daily.java
@@ -3228,12 +3228,58 @@ public class Daily {
             if (i < minutes - 1) {
                 continue;
             }
-            ans1 = Math.max(ans1,s[1]);
+            ans1 = Math.max(ans1, s[1]);
 
             s[grumpy[i]] -= grumpy[i - minutes - 1] == 1 ? customers[i - minutes - 1] : 0;
         }
 
         return s[0] + ans1;
+    }
+
+
+    /**
+     * 4.24
+     * 2385. 感染二叉树需要的总时间
+     *
+     * @param root
+     * @param start
+     * @return
+     */
+    private TreeNode startNode;
+    private final Map<TreeNode, TreeNode> fa = new HashMap<>();
+
+    public int amountOfTime1(TreeNode root, int start) {
+        dfs(root, null, start);
+        return maxDepth(startNode, startNode);
+    }
+
+    private void dfs(TreeNode node, TreeNode from, int start) {
+        if (node == null) {
+            return;
+        }
+        fa.put(node, from); // 记录每个节点的父节点
+        if (node.val == start) {
+            startNode = node; // 找到 start
+        }
+        dfs(node.left, node, start);
+        dfs(node.right, node, start);
+    }
+
+    private int maxDepth(TreeNode node, TreeNode from) {
+        if (node == null) {
+            return -1; // 注意这里是 -1，因为 start 的深度为 0
+        }
+        int res = -1;
+        if (node.left != from) {
+            res = Math.max(res, maxDepth(node.left, node));
+        }
+        if (node.right != from) {
+            res = Math.max(res, maxDepth(node.right, node));
+        }
+        if (fa.get(node) != from) {
+            res = Math.max(res, maxDepth(fa.get(node), node));
+        }
+        return res + 1;
     }
 }
 
