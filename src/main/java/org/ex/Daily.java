7c9b51cbddc333c5ee3d1f1486641c2b82fc2741
Wgz <1808365012@mail.ynu.edu.cn>
2024-04-28 16:12:33 +0800

    04.28 1017. 负二进制转换

--- src/main/java/org/ex/Daily.java
+++ src/main/java/org/ex/Daily.java
@@ -3390,6 +3390,27 @@ public class Daily {
         }
     }
 
+    /**
+     * 04.28
+     * 1017. 负二进制转换
+     *
+     * @param n
+     * @return
+     */
+    public String baseNeg2(int n) {
+        if (n == 0 || n == 1) {
+            return String.valueOf(n);
+        }
+        StringBuilder res = new StringBuilder();
+        while (n != 0) {
+            int remainder = n & 1;
+            res.append(remainder);
+            n -= remainder;
+            n /= -2;
+        }
+        return res.reverse().toString();
+    }
+
 }
 
 
