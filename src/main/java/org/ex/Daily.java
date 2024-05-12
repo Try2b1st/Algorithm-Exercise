27e012f9dd4b804ff81605ff840aecc9264d1382
Wgz <1808365012@mail.ynu.edu.cn>
2024-05-12 13:11:54 +0800

    05.12 1553. 吃掉 N 个橘子的最少天数

--- src/main/java/org/ex/Daily.java
+++ src/main/java/org/ex/Daily.java
@@ -3674,15 +3674,39 @@ public class Daily {
                         break;
                     }
                 }
-                if (!flag){
+                if (!flag) {
                     temp -= travel[i - 1];
-                }else{
+                } else {
                     break;
                 }
             }
             ans += temp;
         }
         return ans;
+    }
+
+
+    /**
+     * 05.12
+     * 1553. 吃掉 N 个橘子的最少天数
+     *
+     * @param n
+     * @return
+     */
+    private final Map<Integer, Integer> memo = new HashMap<>();
+
+    public int minDays(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+        Integer res = memo.get(n);
+        if (res != null) { // 之前计算过
+            return res;
+        }
+        res = Math.min(minDays(n / 2) + n % 2, minDays(n / 3) + n % 3) + 1;
+        memo.put(n, res); // 记忆化
+        return res;
     }
 }
 
