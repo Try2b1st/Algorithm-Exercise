9c9a9bb0f3f017193a7da79d28d7ae218fe2eecc
Wgz <1808365012@mail.ynu.edu.cn>
2024-05-26 13:05:15 +0800

    05.26 1738. 找出第 K 大的异或坐标值

--- src/main/java/org/ex/Daily.java
+++ src/main/java/org/ex/Daily.java
@@ -4002,6 +4002,42 @@ public class Daily {
         }
         return new int[]{-1, -1};
     }
+
+
+    /**
+     * 05.26
+     * 1738. 找出第 K 大的异或坐标值
+     *
+     * @param matrix
+     * @param k
+     * @return
+     */
+    public int kthLargestValue(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        List<Integer> list = new ArrayList<>();
+
+        dp[0][0] = matrix[0][0];
+        list.add(dp[0][0]);
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] ^ matrix[0][i];
+            list.add(dp[0][i]);
+        }
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] ^ matrix[i][0];
+            list.add(dp[i][0]);
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = matrix[i][j] ^ dp[i - 1][j] ^ dp[i][j - 1];
+                list.add(dp[i][j]);
+            }
+        }
+
+        list.sort((a, b) -> b - a);
+        return list.get(k-1);
+    }
 }
 
 
