2a3eef7fea2f9a7a518d05db6b0d0a9f770d92a5
Wgz <1808365012@mail.ynu.edu.cn>
2025-01-09 11:13:26 +0800

    2025.01.09 3297. 统计重新排列后包含另一个字符串的子字符串数目 I

--- src/main/java/org/ex/Daily.java
+++ src/main/java/org/ex/Daily.java
@@ -5537,6 +5537,57 @@ public class Daily {
 
         return result;
     }
+
+    /**
+     * 2025.01.09
+     * 3297. 统计重新排列后包含另一个字符串的子字符串数目 I
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public long validSubstringCount(String word1, String word2) {
+        if (word1.length() < word2.length()) {
+            return 0;
+        }
+
+        char[] s = word1.toCharArray();
+        char[] t = word2.toCharArray();
+        int[] diff = new int[26]; // t 的字母出现次数与 s 的字母出现次数之差
+        for (char c : t) {
+            diff[c - 'a']++;
+        }
+
+        // 统计窗口内有多少个字母的出现次数比 t 的少
+        int less = 0;
+        for (int d : diff) {
+            if (d > 0) {
+                less++;
+            }
+        }
+
+        long ans = 0;
+        int left = 0;
+        for (char c : s) {
+            diff[c - 'a']--;
+            if (diff[c - 'a'] == 0) {
+                // c 移入窗口后，窗口内 c 的出现次数和 t 的一样
+                less--;
+            }
+            while (less == 0) { // 窗口符合要求
+                char outChar = s[left++]; // 准备移出窗口的字母
+                if (diff[outChar - 'a'] == 0) {
+                    // outChar 移出窗口之前检查出现次数，
+                    // 如果窗口内 outChar 的出现次数和 t 的一样，
+                    // 那么 outChar 移出窗口后，窗口内 outChar 的出现次数比 t 的少
+                    less++;
+                }
+                diff[outChar - 'a']++;
+            }
+            ans += left;
+        }
+        return ans;
+    }
 }
 
 
